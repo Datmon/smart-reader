56a10a82ef4496811736b5933b60cfa5e6a4f8da
Данил Анпилов <danilanpilov@MacBook-Air-Danil.local>
2022-01-12 00:15:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import {name as appName} from './app.json';
+import {LanguageContextProvider} from './src/context/LanguageContext';
+
+AppRegistry.registerComponent(appName, () => LanguageContextProvider);
